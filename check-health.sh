0de1a2bec6bd0a85c842329d07852aabdbecc25e
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-03-10 10:30:09 +0100

    Update check

--- check-health.sh
+++ check-health.sh
@@ -3,14 +3,9 @@
 export CHAINWEB_NETWORK=${CHAINWEB_NETWORK:-mainnet01}
 export CHAINWEB_P2P_PORT=${CHAINWEB_P2P_PORT:-1789}
 
-CURRENT_TIME=$(date '+%s')
-BASE_TIME=1645254979
-BASE_HEIGHT=48560472
-TIME_DIFF=$((CURRENT_TIME-BASE_TIME))
-BLOCKS_PASSED_DIFF=$((TIME_DIFF/30*20))
-CURRENT_BLOCK_EST=$((BASE_HEIGHT+BLOCKS_PASSED_DIFF))
-MIN_ACCEPTED_HEIGHT=$((CURRENT_BLOCK_EST-2400))
-CURRENT_NODE_HEIGHT=$(curl -fsLk "https://localhost:$CHAINWEB_P2P_PORT/chainweb/0.0/$CHAINWEB_NETWORK/cut" | jq '.height')
+CURRENT_NODE_HEIGHT=$(curl -fsLk "https://localhost:$CHAINWEB_P2P_PORT/chainweb/0.0/$CHAINWEB_NETWORK/cut" | jq -r '.height')
+if ! egrep -o "^[0-9]+$" <<< "$CURRENT_NODE_HEIGHT" &>/dev/null; then
+  echo "Kadena daemon not responding...."
+  exit 1
+fi
 
-
-if ((CURRENT_NODE_HEIGHT>MIN_ACCEPTED_HEIGHT)); then echo "Node is at block ${CURRENT_NODE_HEIGHT} and meets minimum height of ${MIN_ACCEPTED_HEIGHT}"; else exit 1; fi
